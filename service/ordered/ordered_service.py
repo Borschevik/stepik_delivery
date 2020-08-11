6f5b34236e622769c8308b9293c8dba43420ebcd
Anton <emptybox1267@gmail.com>
2020-08-11 13:40:58 +0300

    Fixing Order service

--- service/ordered/ordered_service.py
+++ service/ordered/ordered_service.py
@@ -1,7 +1,7 @@
 """
 Service for saving orders
 """
-from database.models import Meal, Order
+from database.models import Meal, Order, User
 
 
 class OrderedService:
@@ -22,12 +22,13 @@ class OrderedService:
         Add order todatabase
         """
         order = Order()
+        user_id: int = User.query.filter_by(mail=session["user"]["mail"]).first().id
         order.total_sum = self._form.cart_sum.data
         order.status = "processing"
         order.mail = self._form.mail.data
         order.phone = self._form.phone.data
         order.address = self._form.address.data
-        order.user_id = session["user"]["id"]
+        order.user_id = user_id
         for meal_id in session.get("cart"):
             meal = Meal.query.filter_by(id=meal_id).first()
             order.meals.append(meal)
